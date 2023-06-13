e263e65e8b21f6dd171127268f5d7a00701a4e37
kennedy miller <kennedymiller@ca.rr.com>
2023-06-13 08:21:46 -0700

    Initial Commit

---
+++ to do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to do
+//
+//  Created by Kennedy Miller on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
